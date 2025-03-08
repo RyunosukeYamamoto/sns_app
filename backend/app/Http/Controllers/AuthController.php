0905577d6987ce53b1f1f68909daaa1f555e868e
RyunosukeYamamoto <ryunosukeyamamoto0@gmail.com>
2025-03-08 15:52:18 +0900

    comment

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -14,23 +14,30 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    //  ログイン時はsanctumそんな関係ない (?)
     public function login(Request $request)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             /** @var \App\Models\Account $account **/
-            $account = Auth::user();
-            $token = $account->createToken('AccessToken')->plainTextToken;
+            // $account = Auth::guard('web')->user(); エラーにならない
+            $account = Auth::guard()->user(); // web guardからユーザーを取得してる
+            $token = $account->createToken('AccessToken')->plainTextToken; // sanctum のメソッドcreateTokenでpersonal_access_tokensテーブルにトークンを作成する
             return response()->json(['token' => $token], 200);
         } else {
             return response()->json(['error' => '認証に失敗しました'], 401);
         }
     }
 
+    // routeに書いた [auth:sanctum] のおかげでユーザーが特定できてる。webは関係ない
     public function user(Request $request)
     {
+        // Log::alert(Auth::guard('api')->user()); エラーになる。api guard ではaccountsテーブル内のapi_tokenを見て認証してる。
+        // accountsテーブルにapi_tokenカラムは作ってないから認証できない。その辺の面倒なことを sanctum がやってくれてる。
+        // Log::alert(Auth::guard('web')->user()); 何にも入ってない。web guard はそもそもトークンで認証できない。
         return response()->json(
             [
                 'name' => $request->user()->name,
+                // 'name' => Auth::guard('web')->user()->name, エラーになる
                 'email' => $request->user()->email,
                 'birthday' => $request->user()->birthday,
             ]
